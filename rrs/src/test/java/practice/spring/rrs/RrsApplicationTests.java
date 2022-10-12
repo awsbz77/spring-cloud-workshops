98440f9c4bff93b2ac656691b98e3ce49d26f0e2
zaver <zaver@77-macbook-air.lan>
2022-10-12 15:46:36 -0400

    initial check-in

---
+++ rrs/src/test/java/practice/spring/rrs/RrsApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.spring.rrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
