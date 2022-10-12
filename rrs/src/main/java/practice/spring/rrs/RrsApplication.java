258e1f86b1ac209dd839f4e32012d8c5535afdb9
Zaver <awsbz77@gmail.com>
2022-10-12 17:07:00 -0400

    Added new rest controller

--- rrs/src/main/java/practice/spring/rrs/RrsApplication.java
+++ rrs/src/main/java/practice/spring/rrs/RrsApplication.java
@@ -3,6 +3,11 @@ package practice.spring.rrs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class RrsApplication {
@@ -13,4 +18,13 @@ public class RrsApplication {
         // We need to block for the content here or the JVM might exit before the message is logged
         System.out.println(">> message = " + greetingClient.getMessage().block());
     }
+
+	@RestController
+	class Day1Controller {
+		@GetMapping("/day1")
+		public List<String> day1() {
+			return Arrays.asList("Thursday","Friday");
+
+		}
+	}
 }
